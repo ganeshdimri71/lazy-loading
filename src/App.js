0cf14e7f30afca2141fdd53a60db85ac8e5d1dd8
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-03-16 03:22:46 +0530

    k

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+// import CustomSpeedDail from './CustomSpeedDail';
+import { Suspense, useState } from 'react';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+
+const CustomSpeedDail = React.lazy(() => import('./CustomSpeedDail'))
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [showSpeedDail, setShowSpeedDail] = useState(false)
+  if (showSpeedDail) {
+    return (
+      <Suspense fallback={<CircularProgress />}><CustomSpeedDail /></Suspense>
+    )
+  }
+  return <Button variant='contained'
+    onClick={() => setShowSpeedDail(true)}
+  >Show Speed Dail</Button>
 }
 
 export default App;
